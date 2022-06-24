3d2669e8fda2140ea1611b0a1b31bf6256043cbf
RafinurM <zadorinka16@mail.ru>
2022-06-25 01:48:48 +0700

    Resolve tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,25 @@
 function compareArrays(arr1, arr2) {
   let result;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        result = true;
+      } else {
+        result = false;
+      };
+    };
+  } else {
+    result = false;
+  };
 
-  // Ваш код
-
-  return result; // boolean
+  return result;
 }
 
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr
+    .filter((arr) => arr > 0)
+    .filter((arr) => arr % 3 === 0)
+    .map((arr) => arr * 10);
   return resultArr; // array
 }
