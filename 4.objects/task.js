2c50b9231d961ada23caf2e08117fd3cf2012ee0
RafinurM <zadorinka16@mail.ru>
2022-06-26 04:52:53 +0700

    Resolve task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,33 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  Object.assign(this, { name, gender, age });
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function (...rest) {
+  this.marks = [...rest];
+};
+
+let average = 0;
+Student.prototype.getAverage = function (marks) {
+  this.marks.forEach((item) => {
+    average = average + item
+  })
+  return average/this.marks.length 
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
